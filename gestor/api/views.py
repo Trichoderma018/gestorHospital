678217a433288dc2e77f947af6a66be3f46542d6
juanpa <juan.paq72@mail.com>
2025-05-08 13:34:11 -0600

    todo

--- gestor/api/views.py
+++ gestor/api/views.py
@@ -1,43 +1,58 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Pacientes, Doctores, Citas, Especialidades, DoctoresEspecialidades
 from .serializers import PacientesSerializer, DoctoresSerializer, CitasSerializer, EspecialidadesSerializer, DoctoresEspecialidadesSerializer
+from rest_framework.permissions import BasePermission, IsAuthenticated
+
+class IsAdminUserGroup(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='admin').exists()
 
 class PacientesListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Pacientes.objects.all()
     serializer_class = PacientesSerializer
 
 class PacientesDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUserGroup, IsAuthenticated]
     queryset = Pacientes.objects.all()
     serializer_class = PacientesSerializer
 
 class DoctoresListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Doctores.objects.all()
     serializer_class = DoctoresSerializer
 
 class DoctoresDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUserGroup, IsAuthenticated]
     queryset = Doctores.objects.all()
     serializer_class = DoctoresSerializer
 
 class CitasListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Citas.objects.all()
     serializer_class = CitasSerializer
 
 class CitasDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUserGroup, IsAuthenticated]
     queryset = Citas.objects.all()
     serializer_class = CitasSerializer
 
 class EspecialidadesListCreateView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Especialidades.objects.all()
     serializer_class = EspecialidadesSerializer
 
 class EspecialidadesDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUserGroup, IsAuthenticated]
     queryset = Especialidades.objects.all()
     serializer_class = EspecialidadesSerializer
 
 class DoctoresEspecialidadesListCreateView(ListCreateAPIView):
-    queryset = DoctoresEspecialidades.objects.all()    # Cambiado de Doctores_especialidades a DoctoresEspecialidades
+    permission_classes = [IsAuthenticated]
+    queryset = DoctoresEspecialidades.objects.all()
     serializer_class = DoctoresEspecialidadesSerializer
 
 class DoctoresEspecialidadesDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = DoctoresEspecialidades.objects.all()    # Cambiado de Doctores_especialidades a DoctoresEspecialidades
+    permission_classes = [IsAdminUserGroup, IsAuthenticated]
+    queryset = DoctoresEspecialidades.objects.all()
     serializer_class = DoctoresEspecialidadesSerializer
